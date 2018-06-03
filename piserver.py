ebe3c77e2435e8158821e3f708dce0b5ac043dd9
Yann Bouteiller <yann.bouteiller@etu.emse.fr>
2018-06-03 15:15:16 +0200

    commentaires et lisibilité

--- piserver.py
+++ piserver.py
@@ -9,14 +9,19 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 import json
 import re
 
-VAL_MOTEUR_MIN = -100
-VAL_MOTEUR_MAX = 100
+VAL_MOTEUR_MIN = -100 # valeur minimale de commande prise en compte
+VAL_MOTEUR_MAX = 100 # valeur maximale de commande prise en compte
+COMMANDS = set("forward down frontT backT".split()) # noms des commandes pris en compte
+code_ok_noContent = 204 #code de réussite
+code_error = 400 # code d'erreur
+workingDirectory = "../remote" # dossier contenant la page web à envoyer
+port = 9000 # port par défaut
+host = '' # hote accepté par défaut
 
-COMMANDS = set("forward down frontT backT".split())
-
-commandQueue = mp.SimpleQueue()
+commandQueue = mp.SimpleQueue() # file des actions reçues par le serveur et en attente de traitement
 
 def printAREM(*arg, **kwargs):
+    ''' ajoute "[AREM]" et affiche les données en paramètre '''
     print("[AREM]", *arg, **kwargs)
 
 def info(obj):
@@ -36,23 +41,23 @@ def nope(*args, **kwargs):
 
 
 def action_tell(command):
-    """Print les valeurs des 4 moteurs
-    
-    :param: command Dictionnaire contenant les valeurs pour les clés à metter à jour.
-    """
+    '''
+    Print les valeurs des 4 moteurs
+    :param: command Dictionnaire contenant les valeurs pour les clés à mettre à jour.
+    '''
     for key, value in command.items():
         printAREM("  {}: {}".format(key, value))
     time.sleep(0.2) # seconds
 
 
-def serve_with_action_handler(port=9000, host='', action=nope): # action est l'action qu'on veut voir effectuée avec les informations de POST
+def serve_with_action_handler(port=port, host=host, action=nope): # action est l'action qu'on veut voir effectuée avec les informations de POST
     ''' fonction serveur '''
     class Handler(SimpleHTTPRequestHandler):
         log_message = nope # on rédéfinit la fonction log_message
 
         def do_POST(self):
-            content_length = int(self.headers['Content-Length'])
-            post_body = self.rfile.read(content_length).decode("utf-8")
+            content_length = int(self.headers['Content-Length']) # longueur du body
+            post_body = self.rfile.read(content_length).decode("utf-8") #body
             
             body = json.loads(post_body)
             ok = isinstance(body, dict) # json peut renvoyer plein de trucs, mais on attend un dict
@@ -60,19 +65,17 @@ def serve_with_action_handler(port=9000, host='', action=nope): # action est l'a
             ok = ok and all(isinstance(val, int) and VAL_MOTEUR_MIN <= val <= VAL_MOTEUR_MAX for val in body.values()) # (...for...) est un générateur, all s'en sert
 
             if ok:
-                code_ok_noContent = 204
-                code = code_ok_noContent
+                code = code_ok_noContent # code de réussite
             else:
-                code_error = 400
-                code = code_error
+                code = code_error # code d'erreur
             
             printAREM("POST / {}:".format(code))
-            self.send_response(code)
-            self.end_headers()
+            self.send_response(code) # envoi du code de réponse
+            self.end_headers() # envoi de la réponse (fin)
 
             if ok:
                 command = body
-                commandQueue.put(command)
+                commandQueue.put(command) # commande ajoutée à la file d'attente
 
     def serve():
         server_address = (host, port)
@@ -86,20 +89,17 @@ def serve_with_action_handler(port=9000, host='', action=nope): # action est l'a
 
     def queueRunner():
         while True:
-            command = commandQueue.get() # Probably bloking
+            command = commandQueue.get() # Probably blocking
             action(command)
     
-    pqr = mp.Process(target=queueRunner)
-    pqr.start()
-    serve()
-    pqr.join()
+    pqr = mp.Process(target=queueRunner) # nouveau process : execution des commandes dans la file d'attente
+    pqr.start() # démarrage du process pqr
+    serve() # démarrage du serveur
+    pqr.join() # fin du process pqr
 
 if __name__ == '__main__': # sert à savoir si on est utilisé comme module ou comme programme principal
     from sys import argv
-    workingDirectory = "../remote"
-    port = 9000
-    host = ''
-    if len(argv) >= 2: # on regarde s'il y a au moins un argument
+    if len(argv) >= 2:
         workingDirectory = argv[1]
         if len(argv) >= 3:
             port = int(argv[2])
@@ -107,4 +107,4 @@ if __name__ == '__main__': # sert à savoir si on est utilisé comme module ou c
                 host = argv[3]
     os.chdir(workingDirectory)
 
-    serve_with_action_handler(port=port, host=host, action=action_tell)
+    serve_with_action_handler(port=port, host=host, action=action_tell) # lancement du serveur
